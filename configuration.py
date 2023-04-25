d3197a6d57c6242492457c7978b468122ec6952c
DESKTOP-0OMR998\Kalevala <kalevalaak@ya.ru>
2023-04-25 15:04:34 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://b5783b8f-4e32-4cc0-bf8d-a6e75ac40c4d.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
